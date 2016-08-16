95e8b8be0365040b49c384e953c0b7a239566029
danx_ <danx_@192.168.1.74>
2016-08-15 21:29:05 -0500

    Added comments.

--- src/ed/dmartínezc/t01/NumeroElevado.java
+++ src/ed/dmartínezc/t01/NumeroElevado.java
@@ -17,10 +17,23 @@ public class NumeroElevado {
         
     }
     
+    /**
+     * 
+     * @param base, la base que va a ser elevado
+     * @param exp, el numero de veces que se elevara la base
+     * @return base (se va multiplicando por si misma en cada ciclo recursivo)
+     */
     public static Double Elevar(Double base, int exp) {
+        /**
+         * Caso Base: Cuando el exponente llegue a uno regresara la base
+         */
         if (exp == 1) {
             return base;
         } else {
+            /**
+             * Dominio: la base se va multiplicando por si misma, restandole 1
+             * al exponente cada vez que un ciclo recursivo pasa.
+             */
             return base * Elevar(base, (exp - 1));
         }
     }
